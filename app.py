be6402b199feac95053e42a7865c0f499e112068
michelleherman13 <85762953+michelleherman13@users.noreply.github.com>
2021-12-07 16:02:19 -0600

    update web pgs, add requirements.txt, update basic flask

--- app.py
+++ app.py
@@ -1,83 +1,14 @@
-from flask import Flask, render_template, jsonify, send_from_directory, request
-import json
-import pandas as pd
-import numpy as np
-import os
-from modelHelper import ModelHelper
+from flask import Flask
+from flask import render_template
 
-#init app and class
+# creates a Flask application, named app
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-modelHelper = ModelHelper()
 
-#endpoint
-# Favicon
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
-
-# Route to render index.html template
+# a route where we will display a welcome message via an HTML template
 @app.route("/")
-def home():
-    # Return template and data
-    return render_template("index.html")
+def hello():
+    return render_template('index.html')
 
-# Route to render index.html template
-@app.route("/about")
-def about():
-    # Return template and data
-    return render_template("about_us.html")
-    
-@app.route("/tableau")
-def tableau():
-    # Return template and data
-    return render_template("tableau.html")
-
-
-@app.route("/makePredictions", methods=["POST"])
-def makePredictions():
-    content = request.json["data"]
-
-    # parse
-    sex_flag = int(content["sex_flag"])
-    age = float(content["age"])
-    fare = float(content["fare"])
-    familySize = int(content["familySize"])
-    p_class = int(content["p_class"])
-    embarked = content["embarked"]
-
-    # #dummy data
-    # sex_flag = 1
-    # age = 25
-    # fare = 25
-    # familySize = 2
-    # p_class = 1
-    # embarked = "C"
-
-    prediction = modelHelper.makePredictions(sex_flag, age, fare, familySize, p_class, embarked)
-    print(prediction)
-    return(jsonify({"ok": True, "prediction": str(prediction)}))
-
-####################################
-# ADD MORE ENDPOINTS
-
-###########################################
-
-#############################################################
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    return r
-
-#main
+# run the application
 if __name__ == "__main__":
     app.run(debug=True)
