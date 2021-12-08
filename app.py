047cc690b5dbc5f8378719d0b1d26de8adbdf6da
michelleherman13 <85762953+michelleherman13@users.noreply.github.com>
2021-12-07 23:58:53 -0600

    update app.py tableau.html route

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import render_template
-
 # creates a Flask application, named app
 app = Flask(__name__)
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def home():
+    return render_template("index.html")
+
+@app.route("/tableau")
+def tableau():
+    # Return template and data
+    return render_template("tableau.html")
 
 # run the application
 if __name__ == "__main__":
